7902346d0e0e55e7b3ebe761ddf047dfc6d78969
zolnikov.vv <zolnikov.vv@prg-it.ru>
2025-12-14 21:46:14 +0500

    Initial commit

---
+++ flask_redis/app.py
@@ -0,0 +1,33 @@
+import socket
+import redis
+from flask import Flask, make_response
+
+app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+
+
+def get_hit_count() -> int:
+    return int(cache.get('hits') or 0)
+
+
+def incr_hit_count() -> int:
+    return cache.incr('hits')
+
+
+@app.route('/metrics')
+def metrics():
+    metrics_text = f'''
+# HELP view_count Flask-Redis-App visit counter
+# TYPE view_count counter
+view_count{{service="Flask-Redis-App"}} {get_hit_count()}
+'''
+    response = make_response(metrics_text, 200)
+    response.mimetype = "text/plain"
+    return response
+
+
+@app.route('/')
+def hello():
+    incr_hit_count()
+    count = get_hit_count()
+    return f'Hello World! I have been seen {count} times. My name is: {socket.gethostname()}\n'
